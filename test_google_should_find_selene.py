85a1ae6a59d04ed8a578015f0ed3ee74aaceb673
Vados61 <pvild3d@gmail.com>
2022-12-12 17:15:34 +0300

    refactor test by pytest

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,7 +1,23 @@
+import pytest as pytest
 from selene.support.shared import browser
 from selene import be, have
 
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+@pytest.fixture(scope="session")
+def open_browser():
+    browser.config.window_width = 500
+    browser.config.window_height = 500
+    browser.config.hold_browser_open = True
+    browser.open('https://google.com/ncr')
+    yield browser
+    browser.quit()
+
+
+def test_find_selene(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_negative_search(open_browser):
+    browser.element('[name="q"]').clear().type('dflkjfgklj').press_enter()
+    browser.element('body').should(have.text('No results containing all your search terms were found'))
